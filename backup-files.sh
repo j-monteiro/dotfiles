4a24a2ce869f2ae007c4ddb8e7fe22511f90bb89
= <=>
2024-05-02 22:28:55 +0100

    chore: include i3 and i3status config

--- backup-files.sh
+++ backup-files.sh
@@ -9,4 +9,6 @@ rsync -a --delete ~/.config/coc ./.config
 rsync -a --delete ~/.config/nvim ./.config
 rsync -a --delete ~/.config/solargraph ./.config
 rsync -a --delete ~/.config/iterm2 ./.config
+rsync -a --delete ~/.config/i3 ./.config
+rsync -a --delete ~/.config/i3status ./.config
 rsync -a --delete ~/.aliases/* ./.aliases
